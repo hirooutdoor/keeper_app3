49b232938baee454ecab9df0211f090aa708f811
hiro <hmaeyama0510@gmail.com>
2021-05-22 05:43:42 +0900

    Create a constant that keeps track of the title and content.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div>
       <Header />
       <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <Note key={1} title={noteTitle} content={noteContent} />
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateArea() {
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputContent, setInputContent] = useState("");
+
+  function handleTitle(event) {
+    const newTitleValue = event.target.value;
+    setInputTitle(newTitleValue);
+  }
+
+  function handleContent(event) {
+    const newTitleValue = event.target.value;
+    setInputContent(newTitleValue);
+  }
+
+  function addItem() {
+    setInputTitle("");
+    setInputText("");
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          onChange={handleTitle}
+          name="title"
+          placeholder="Title"
+          value={inputTitle}
+        />
+        <textarea
+          onChange={handleContent}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={inputContent}
+        />
+        <button onClick={addItem}>Add</button>
       </form>
     </div>
   );
